7d9a2d74fb716dd2ae1363b149f462430933200c
MickyC <47837068+mickychenchen@users.noreply.github.com>
2022-09-04 19:02:40 +1000

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -52,7 +52,7 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: '🛖',
+      name: 'fort-awesome',
       link: 'https://mickyc.gq',
     },
     {
