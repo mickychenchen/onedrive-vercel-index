fd05234dc8be1f7f00e8d4c5bc84df28b2f8f962
MickyC <47837068+mickychenchen@users.noreply.github.com>
2022-09-04 18:34:30 +1000

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -52,11 +52,11 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
+      name: 'House',
+      link: 'https://mickyc.gq',
     },
     {
-      name: 'Telegram',
+      name: "",
       link: 'https://t.me/realSpencerWoo',
     },
   ],
