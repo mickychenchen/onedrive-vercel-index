f7cc28b20237bf4c6298e927d415f1be5228d427
MickyC <47837068+mickychenchen@users.noreply.github.com>
2022-09-04 18:48:44 +1000

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    '<a href="https://mickyc.gq/" target="_blank" rel="noopener noreferrer">MickyC🛖',
+    'Made with ❤ by MickyC',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
@@ -52,7 +52,7 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'fort-awesome-alt',
+      name: '🛖',
       link: 'https://mickyc.gq',
     },
     {
